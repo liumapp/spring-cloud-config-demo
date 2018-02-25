5aa3bef47e0f5bf9a8e5fe3c8c093d430fd277b4
liumapp <liumapp.com@gmail.com>
2018-02-25 13:21:12 +0800

    add cloud config dependency

--- config-demo-server/src/main/java/com/liumapp/demo/config/server/Main.java
+++ config-demo-server/src/main/java/com/liumapp/demo/config/server/Main.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 2/25/18 1:20 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
+@SpringBootApplication(scanBasePackages = {"com.liumapp.demo.config.server"})
 public class Main {
 
     public static void main(String[] args) {
