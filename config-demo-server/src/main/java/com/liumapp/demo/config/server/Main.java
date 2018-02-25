6d0e353cce327a6901f4b172e254b495a47b38b1
liumapp <liumapp.com@gmail.com>
2018-02-25 13:57:08 +0800

    conf server

--- config-demo-server/src/main/java/com/liumapp/demo/config/server/Main.java
+++ config-demo-server/src/main/java/com/liumapp/demo/config/server/Main.java
@@ -2,6 +2,7 @@ package com.liumapp.demo.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by liumapp on 2/25/18 1:20 PM.
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * home-page:http://www.liumapp.com
  */
 @SpringBootApplication(scanBasePackages = {"com.liumapp.demo.config.server"})
+@EnableConfigServer
 public class Main {
 
     public static void main(String[] args) {
