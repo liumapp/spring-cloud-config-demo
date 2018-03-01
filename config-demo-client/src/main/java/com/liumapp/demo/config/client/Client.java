fb8c96bbffe00117ff0307225b3c57706cb2d3a7
liumapp <liumapp.com@gmail.com>
2018-03-01 10:47:56 +0800

    conf eureka done

--- config-demo-client/src/main/java/com/liumapp/demo/config/client/Client.java
+++ config-demo-client/src/main/java/com/liumapp/demo/config/client/Client.java
@@ -2,6 +2,7 @@ package com.liumapp.demo.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by liumapp on 2/25/18 5:44 PM.
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * home-page:http://www.liumapp.com
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Client {
 
     public static void main(String[] args) {
--- config-demo-eureka/src/main/java/com/liumapp/demo/config/eureka/Main.java
+++ config-demo-eureka/src/main/java/com/liumapp/demo/config/eureka/Main.java
@@ -2,13 +2,15 @@ package com.liumapp.demo.config.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 3/1/18 10:44 AM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
+@SpringBootApplication
+@EnableEurekaServer
 public class Main {
 
     public static void main(String[] args) {
